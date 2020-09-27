6f96da9d20f9f8f07e26a98a542ff77d19b3e698
NickMakeThing <nicksegalwork@gmail.com>
2020-09-27 14:55:54 +1000

    added tests. all working.

--- backend/rockpaperscissors/match_maker_utils.py
+++ backend/rockpaperscissors/match_maker_utils.py
@@ -1,11 +1,12 @@
 from .models import Match, PlayerMatch
+from django.db import transaction
 from random import randint
 import hashlib
 
 def create_random_string():
     r_string=''
     for i in range(10):
-        r_string+=chr(randInt(97,122))
+        r_string+=chr(randint(97,122))
     return r_string
 
 def create_match_name(string):
@@ -13,52 +14,52 @@ def create_match_name(string):
     return hashlib.md5(arg).hexdigest()
     
 def create_matches(matches):
+    #var name spaces all fucked up. how to know what to name what? all very similar things.
     random_string = create_random_string()
-    match_entries = []
-    player_match_entries = []
+    player_matches = []
 
-    for i in matches:
-        player1 = i[0].user; player2 = i[1].user
-        hash_input = random_string + player1.username + player2.username
-        match_name = create_match_name(hash_input)
+    with transaction.atomic():
+        for i in matches:
+            player1 = i[0].user; player2 = i[1].user
+            hash_input = random_string + player1.username + player2.username
+            match_name = create_match_name(hash_input)
 
-        match_object = Match(name=match_name)
-        match_entries.append(match_object)
+            match_object = Match.objects.create(name=match_name)
 
-        player_match_entries.append(PlayerMatch(user=player1,match=match_object))
-        player_match_entries.append(PlayerMatch(user=player2,match=match_object))
+            player_matches.append(PlayerMatch.objects.create(user=player1,match=match_object))
+            player_matches.append(PlayerMatch.objects.create(user=player2,match=match_object))
 
-    Match.objects.bulk_create(match_entries)
-    PlayerMatch.objects.bulk_create(player_match_entries)
-        
 def match_players_if_they_map_to_eachother(closest_scores):
     matches=[]
-    for i in closest_scores:
-        if closest_scores.get(closest_scorest.get(i)) == i:
-            matches.append([i,closest_scores.get(i)])
+    closest_scores_copy=closest_scores.copy()
+    for k,v in closest_scores.items():
+        if v in closest_scores_copy and closest_scores_copy[v] == k:
+            del closest_scores_copy[k]
+
+    for k,v in closest_scores_copy.items():
+        if closest_scores[v] == k:
+            matches.append([k,v])
     return matches
            
 def map_player_to_player_of_closest_score(waiting):
-    closest_scores = []
+    closest_scores = {}
     for i in waiting:
         index = waiting.index(i)
-        previous = waiting[index-1]
-        next = waiting[index+1]
         if index != 0:
-            previous_score = abs(previous.score - i.score)
+            previous_score = abs(waiting[index-1].score - i.score)
         else:
-            closest_scores.append({i:next})
+            closest_scores[i] = waiting[index+1]
             continue
         if index != len(waiting)-1:
-            next_score = abs(next.score - i.score)
+            next_score = abs(waiting[index+1].score - i.score)
         else:
-            closest_scores.append({i:previous})
+            closest_scores[i] = waiting[index-1]
             continue
         if previous_score < next_score:
-            closest_scores.append({i:previous})
+            closest_scores[i] = waiting[index-1]
         elif previous_score == next_score:
-            random_pick = random.choice([previous,next])
-            closest_scores.append({i:random_pick})
+            random_pick = random.choice([waiting[index-1],waiting[index+1]])
+            closest_scores[i] = random_pick
         else:
-            closest_scores.append({i:next})
+            closest_scores[i] = waiting[index+1]
     return closest_scores
--- backend/rockpaperscissors/tests/test_match_maker_utils.py
+++ backend/rockpaperscissors/tests/test_match_maker_utils.py
@@ -1,31 +1,88 @@
 import pytest
 import rockpaperscissors.match_maker_utils as utils
 from rockpaperscissors.models import Match, PlayerMatch, PlayerStatus
+from django.contrib.auth.models import User
+
+def _get_test_data():
+    player_data = [
+        ["Matt",100],
+        ["Nick",111],
+        ["Bob",120],
+        ["Joe",135],
+        ["Sally",140],
+        ["Sam",150]
+    ]
+
+    for username, score in player_data:
+        user = User.objects.create(username=username)
+        PlayerStatus.objects.create(user=user, score=score, looking_for_opponent=True)
+
+    players = PlayerStatus.objects.all()
+    players = list(players)
+
+    mappings = {
+            players[0]:players[1],
+            players[1]:players[2],
+            players[2]:players[1],
+            players[3]:players[4],
+            players[4]:players[3],
+            players[5]:players[4]
+        }
+
+    matches = [
+        [players[2],players[1]],
+        [players[4],players[3]],
+    ]
+    
+    player_matches = [
+        {'user':'Bob','match':1,'score':0},
+        {'user':'Nick','match':1,'score':0},
+        {'user':'Sally','match':2,'score':0},
+        {'user':'Joe','match':2,'score':0}
+    ]
+
+    return {
+        'players': players,
+        'mappings': mappings,
+        'matches': matches,
+        'player_matches': player_matches
+    }
+
 
 @pytest.mark.django_db
 def test_map_player_to_player_of_closest_score():
-    pass
+    test_data = _get_test_data()
+    arg = test_data['players']
+    result = test_data['mappings']
+    assert utils.map_player_to_player_of_closest_score(arg) == result
 
 @pytest.mark.django_db
 def test_match_players_if_they_map_to_eachother():
-    pass
+    test_data = _get_test_data()
+    arg = test_data['mappings']
+    result = test_data['matches']
+    assert utils.match_players_if_they_map_to_eachother(arg) == result 
 
 @pytest.mark.django_db
 def test_create_matches():
-    pass
+    test_data = _get_test_data()
+    arg = test_data['matches']
+    result = test_data['player_matches']
+    utils.create_matches(arg)
+    player_matches = PlayerMatch.objects.all()
+    matches = Match.objects.all()
+    assert matches.count() == 2
+    assert player_matches.count() == 4
+    assert player_matches[0].match == player_matches[1].match
+    assert player_matches[2].match == player_matches[3].match
+    assert player_matches[1].match != player_matches[2].match
+    for row,testcase in zip(player_matches,result):
+        assert row.user.username == testcase['user']
+        assert row.match.id == testcase['match']
+        assert row.game_score == testcase['score']
 
-@pytest.mark.django_db
-def test_data():
-    test_data = [
-        {"user": "Matt", "score":100,"looking_for_opponent": True},
-        {"user": "Nick", "score":111,"looking_for_opponent": True},
-        {"user": "Bob", "score":120,"looking_for_opponent": True},
-        {"user": "Joe", "score":135,"looking_for_opponent": True},
-        {"user": "Sally", "score":140,"looking_for_opponent": True},
-        {"user": "Sam", "score":150,"looking_for_opponent": True},
-    ]
+        
+        
 
-    PlayerStatus.objects.bulk_create(test_data)
+    
 
-    data = PlayerStatus.objects.all()
-    print(data)
