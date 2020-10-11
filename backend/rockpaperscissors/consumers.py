4f3aee1af563d5bb4be911def6084a3c486bc926
NickMakeThing <nicksegalwork@gmail.com>
2020-10-11 19:42:52 +1100

    forgot to save consumer.py before last push.

--- backend/rockpaperscissors/consumers.py
+++ backend/rockpaperscissors/consumers.py
@@ -12,9 +12,12 @@ def look_for_match(player, self_instance):
         player_match = PlayerMatch.objects.filter(player=player.id)
         if player_match.exists():
             player.looking_for_opponent = False
-            player_match = player_match.first()
+            match = player_match.first().match
+            opponent = PlayerMatch.objects.filter(match=match).exclude(player=player).first() #could give 'opponent' field in playermatch
+            opponent = opponent.player.name # way too many queries
             self_instance.send(text_data=json.dumps({
-                "match_name": player_match.match.name #make it so this doesnt do 2 queries
+                "match_name": player_match.name,
+                "opponent": opponent
             }))
         sleep(2)
 
@@ -51,6 +54,8 @@ class GameUpdateConsumer(WebsocketConsumer):
     def connect(self):
         self.match = self.scope['path'][10:]# [10:-1] if leading /, also can split by / get last.
         contestants = PlayerMatch.objects.filter(name=self.match)
+        name=self.scope['cookies'].keys()[0] #change because error will happen when other cookies are present.
+        player = PlayerStatus.objects.get(name=name) #too many queries.
         if contestants.exists():
             #check if playermatch has match
             #check cookie?
@@ -58,9 +63,11 @@ class GameUpdateConsumer(WebsocketConsumer):
                 self.match,
                 self.channel_name
             )
-            self.accept()
-            self.scope['cookies']
-            self.send(text_data=json.dumps(update))
+            self.accept() 
+
+            
+            opponent = contestants.exclude(player=player).first() #too many queries.
+            self.send(text_data=json.dumps(oppnent.player.name)) #too many queries.
             print('match found')
         else: 
             print('no match found')
